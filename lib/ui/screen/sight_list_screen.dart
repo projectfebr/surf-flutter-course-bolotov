2618a953b00c919f3355a0c48682fe696765d174
“Aleksandr <projectfebr@gmail.com>
2021-08-24 23:16:46 +0300

    Text и TextStyle

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,9 +10,28 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Center(
+      appBar: AppBar(
+        // 2) Уберите тени
+        shadowColor: Colors.transparent,
+        // 1) Задайте цвет аппбара идентично цвету заднего фона
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        // 3)Задайте высоту аппбара так, чтобы текст не вылезал за края виджета
+        toolbarHeight: 100,
+        title: const Text(
+          'Список интересных мест',
+          maxLines: 2,
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 32,
+          ),
+        ),
+      ),
+      body: const Center(
         child: Text('Hello'),
       ),
     );
