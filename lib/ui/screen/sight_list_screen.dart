63ed889c18063ce85dca49b264d943b83b66f80b
“Aleksandr <projectfebr@gmail.com>
2021-08-24 23:33:50 +0300

    RichText

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -19,17 +19,41 @@ class _SightListScreenState extends State<SightListScreen> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         // 3)Задайте высоту аппбара так, чтобы текст не вылезал за края виджета
         toolbarHeight: 100,
-        title: const Text(
-          'Список интересных мест',
-          maxLines: 2,
-          textAlign: TextAlign.start,
-          style: TextStyle(
-            fontFamily: 'Roboto',
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 32,
+        centerTitle: false,
+        title: RichText(
+          text: const TextSpan(
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+            ),
+            children: [
+              TextSpan(children: [
+                TextSpan(
+                    text: 'C',
+                    style: TextStyle(color: Color.fromRGBO(76, 175, 80, 1.0))),
+                TextSpan(text: 'писок\n'),
+              ]),
+              TextSpan(children: [
+                TextSpan(
+                    text: 'и',
+                    style: TextStyle(color: Color.fromRGBO(252, 221, 61, 1.0))),
+                TextSpan(text: 'нтересных мест'),
+              ]),
+            ],
           ),
         ),
+        // const Text(
+        //   'Список интересных мест',
+        //   maxLines: 2,
+        //   textAlign: TextAlign.start,
+        //   style: TextStyle(
+        //     fontFamily: 'Roboto',
+        //     color: Colors.black,
+        //     fontWeight: FontWeight.bold,
+        //     fontSize: 32,
+        //   ),
       ),
       body: const Center(
         child: Text('Hello'),
