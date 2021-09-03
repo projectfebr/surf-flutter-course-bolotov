a9763ed8badb56b188ae28cd5b268acc939703d2
“Aleksandr <projectfebr@gmail.com>
2021-09-03 15:26:10 +0300

    - в class Sight добавлено поле imageUrl - фотограция, тип, иконка обернуты в стэк

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -5,6 +5,8 @@ final List<Sight> mocks = <Sight>[
       name: 'Смотровая площадка «Семь ветров»',
       lat: 44.725662287326365,
       lon: 37.867255241955114,
+      imageUrl:
+          'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/f0/a1/f6/caption.jpg?w=2000&h=-1&s=1',
       url:
           'https://www.tripadvisor.ru/Attraction_Review-g660729-d6429400-Reviews-Observation_deck_7_winds-Novorossiysk_Krasnodar_Krai_Southern_District.html',
       details:
@@ -14,6 +16,8 @@ final List<Sight> mocks = <Sight>[
     name: 'Озеро Абрау',
     lat: 44.700292323276116,
     lon: 37.59190615885557,
+    imageUrl:
+        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/cc/b6/cc/caption.jpg?w=1400&h=-1&s=1',
     url:
         'https://www.tripadvisor.ru/Attraction_Review-g660729-d7240720-Reviews-Lake_Abrau-Novorossiysk_Krasnodar_Krai_Southern_District.html',
     details:
